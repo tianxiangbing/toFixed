31e337b32103c8d1bd50781687b13af34c4bff77
tianxiangbing <55342775@qq.com>
2017-07-25 18:09:32 +0800

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+Number.prototype.toFixed = function (d) {
+    var s = this + "";
+    if (!d) d = 0;
+    d = parseInt(d);
+    if (s.indexOf(".") == -1) s += ".";
+    s += new Array(d + 1).join("0");
+    if (new RegExp("^(-|\\+)?(\\d+(\\.\\d{0," + (d + 1) + "})?)\\d*$").test(s)) {
+        var s = "0" + RegExp.$2, pm = RegExp.$1, a = RegExp.$3.length, b = true;
+        if (a == d + 2) {
+            a = s.match(/\d/g);
+            if ((pm !== '-' && parseInt(a[a.length - 1]) > 4) || (pm === "-" && parseInt(a[a.length - 1]) > 5)) {
+                for (var i = a.length - 2; i >= 0; i--) {
+                    a[i] = parseInt(a[i]) + 1;
+                    if (a[i] == 10) {
+                        a[i] = 0;
+                        b = i != 1;
+                    } else break;
+                }
+            }
+            s = a.join("").replace(new RegExp("(\\d+)(\\d{" + d + "})\\d$"), "$1.$2");
+        } if (b) s = s.substr(1);
+        return (pm + s).replace(/\.$/, "");
+    } return this + "";
+};
