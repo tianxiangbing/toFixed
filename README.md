705244fb934edbda1fa698e1bd4e844f70f84c45
tianxiangbing <55342775@qq.com>
2017-07-31 13:57:25 +0800

    0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# toFixed
-js浮点数的toFixed四舍五入,负数与正数执行相同的逻辑。-1.5返回-1,-1.6返回-2
+# toFixed(len)
+    js浮点数的toFixed四舍五入,负数与正数执行相同的逻辑。-1.5返回-1,-1.6返回-2
+# toFixedMax(max) 
+    最大小数位数 ,尾数为0的忽略
+
+# NPMJS
+    npm install js-tofixed --save
--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-"use strict";
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-_extends(Number.prototype, {
-    toFixedMax: function toFixedMax(max) {
+/*
+ * Created with Visual Studio Code.
+ * github: https://github.com/tianxiangbing/toFixed.git
+ * User: 田想兵
+ * Date: 2017-07-31
+ * Time: 20:00:00
+ * Contact: 55342775@qq.com
+ * desc: 对小数位的四舍五入处理。
+ * 请使用https://github.com/tianxiangbing/toFixed.git 上的代码
+ */
+Object.assign(Number.prototype, {
+    toFixedMax(max) {
+        let ps = this.split('.');
+        let len= 0 ;
+        if(ps.length>1){
+            len = ps[1].length;
+        }
+        max = Math.min(len,max);
         return this.toFixed(max);
     },
-    toFixed: function toFixed(d) {
+    toFixed(d) {
         var s = this + "";
         if (!d) d = 0;
         d = parseInt(d);
         if (s.indexOf(".") == -1) s += ".";
         s += new Array(d + 1).join("0");
         if (new RegExp("^(-|\\+)?(\\d+(\\.\\d{0," + (d + 1) + "})?)\\d*$").test(s)) {
-            var s = "0" + RegExp.$2,
-                pm = RegExp.$1,
-                a = RegExp.$3.length,
-                b = true;
+            var s = "0" + RegExp.$2, pm = RegExp.$1, a = RegExp.$3.length, b = true;
             if (a == d + 2) {
                 a = s.match(/\d/g);
-                if (pm !== '-' && parseInt(a[a.length - 1]) > 4 || pm === "-" && parseInt(a[a.length - 1]) > 5) {
+                if ((pm !== '-' && parseInt(a[a.length - 1]) > 4) || (pm === "-" && parseInt(a[a.length - 1]) > 5)) {
                     for (var i = a.length - 2; i >= 0; i--) {
                         a[i] = parseInt(a[i]) + 1;
                         if (a[i] == 10) {
@@ -29,8 +38,8 @@ _extends(Number.prototype, {
                     }
                 }
                 s = a.join("").replace(new RegExp("(\\d+)(\\d{" + d + "})\\d$"), "$1.$2");
-            }if (b) s = s.substr(1);
+            } if (b) s = s.substr(1);
             return (pm + s).replace(/\.$/, "");
-        }return this + "";
+        } return this + "";
     }
-});
+})
